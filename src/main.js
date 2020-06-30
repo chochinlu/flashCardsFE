2935d6f31199b8b951fa78ea40972af2a1decee8
Park.Lu <park.lu@nogle.com>
2020-06-30 18:24:28 +0800

    add basic login - update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
-import AxiosPlugin from 'vue-axios-cors';
 import App from './App.vue'
 
 Vue.config.productionTip = false
-Vue.use(AxiosPlugin)
 
 new Vue({
   render: h => h(App),
